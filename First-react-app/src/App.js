8fc6ebef13e81d39cdc8f19ec9c93271e34fc8dc
Pratik17SV <pratik.dg@somaiya.edu>
2024-12-18 22:54:36 +0530

    creadted new React app and add the components and Screen folder and add the components and screen in it.

---
+++ First-react-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './screen/about';
+import Contact from './screen/Contact';
+
+function App() {
+  return (
+    <div id="app">
+    <About/>
+    <Contact/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ First-react-app/src/screen/about.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Title from "../components/Title";
+import Unorderedlist from "../components/Unorderedlist";
+
+function about(){
+    return(
+        <div id ="about">
+            <Title name="Node JS guide"/>
+            {/* <Unorderedlist/> */}
+        </div>
+        
+    );
+}
+
+export default about;
+
+
