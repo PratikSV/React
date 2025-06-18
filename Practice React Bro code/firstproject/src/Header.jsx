0b3cb76a06bc49a4328b6fbe6592f955facb9970
PratikSV <pratikgaikwad7738@gmail.com>
2025-06-18 20:21:22 +0530

    first video of components done with short java script
    Practice components form the video
    "https://youtu.be/hn80mWvP-9g?si=hc5t4nh0flDyjZGU"

---
+++ Practice React Bro code/firstproject/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul><a href="#">Home</a></ul>
+                <ul><a href="@">About</a></ul>
+                <ul><a href="#">Contact</a></ul>
+                <ul><a href="#">Services</a></ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
